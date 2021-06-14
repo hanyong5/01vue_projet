a2809bfc49e3b4910c780d6bcb2cf28b3d14d183
hanyong5 <hanyong5@naver.com>
2021-06-14 13:26:40 +0900

    img add

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 export default [{
     id : 0,
     title: "디스크빌 751",
-    image: "./img/1715.jpg",
+    image: "https://boot3-hanyong5.netlify.app/images/nano1.jpg",
     content: "프리미엄 클래식 로드자전거",
     price: 677000
     },
     {
     id : 1,
     title: "세인트 버킨",
-    image: "http://www.winoz.co.kr/img/jaike_1650.jpg",
+    image: "https://boot3-hanyong5.netlify.app/images/nano2.jpg",
     content: "오리지널 유럽 더치바이크 남성 클래식자전거",
     price: 307000
     },
